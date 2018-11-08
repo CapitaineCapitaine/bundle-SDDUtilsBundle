89198a33fb5758ad95a786755c972bbae36d5d66
Samuel Denis-D'Ortun <sam@sddproductions.com>
2018-11-08 16:25:19 -0500

    Updated CORS

--- src/CORSHeaderListener.php
+++ src/CORSHeaderListener.php
@@ -1,13 +1,49 @@
 <?php
 namespace SDD\UtilsBundle;
 
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
-class CORSHeaderListener
+class CORSHeaderListener implements EventSubscriberInterface
 {
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST  => array('onKernelRequest', 9999),
+            KernelEvents::RESPONSE => array('onKernelResponse', 9999),
+        );
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        // Don't do anything if it's not the master request.
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $request = $event->getRequest();
+        $method  = $request->getRealMethod();
+        if ('OPTIONS' == $method) {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+
     public function onKernelResponse(FilterResponseEvent $event)
     {
-        $event->getResponse()->headers->set('Access-Control-Allow-Origin', '*');
+        // Don't do anything if it's not the master request.
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $event->getResponse()->headers->set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, Access-token');
     }
 }
 
